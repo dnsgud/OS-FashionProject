c72bf9489e23d0e9c534bfaf7fb283595eda5010
dnsgud <68136582+dnsgud@users.noreply.github.com>
2026-04-07 17:23:03 +0900

    스타일 점수 계산 함수 추가
    TPO를 입력 받고 TPO가 일치할 경우 추가적인 점수를 주는 형식이다.

--- recommend_clothes.py
+++ recommend_clothes.py
@@ -37,20 +37,36 @@ def get_top_combinations(clothes_db, target_lv):
                 valid_combos.append([top, outer])
                 
     return valid_combos
+
+# [스타일 점수 계산 함수]
+def calculate_style_score(full_outfit, target_tpo):
+    """전체 코디(상의+하의)의 TPO 일치도를 계산"""
+    score = 0
+    for cloth in full_outfit:
+        if target_tpo in cloth.get('tpo', []):
+            score += 1
+    return score
         
 # [옷 추천 함수](current_temp 테스트 과정 중 입력으로 대체)
-def recommend_clothes(current_temp, clothes_db):
+def recommend_clothes(current_temp, target_tpo, clothes_db):
+    print(f"\n 기온: {current_temp}°C | 목적: {target_tpo}")
+    
+    # 목표 온도 레벨 계산
     target_lv = get_target_level(current_temp)
+    print(f"목표 온도 레벨: Level {target_lv}\n")
+
+    # 조건에 맞는 하의/상의 조합 가져오기
+    valid_bottoms = get_bottoms(clothes_db, target_lv)
+    valid_top_combos = get_top_combinations(clothes_db, target_lv)
+
+    outfits = []    # 최종 코디 저장 리스트
     
-    tops = [c for c in clothes_db if c['main_category'] == '상의']
-    bottoms = [c for c in clothes_db if c['main_category'] == '하의']
-    
-    print(f"목표 레벨: {target_lv}")
     for top in tops:
         for bottom in bottoms:
             if top['temp_level'] == target_lv:
                 print(f"추천: {top['name']} + {bottom['name']}")
 
-#테스트 입력
+# 테스트 환경
 temp = int(input("현재 기온 입력(예: 14): "))
-recommend_clothes(temp, clothes_db)
+style = input("TPO 입력(예: 캐주얼, 미니멀, 포멀 등): ")
+recommend_clothes(temp, style, mock_db)
