1b61623a374f92a2c9cc1111178f5b38097117a0
dnsgud <68136582+dnsgud@users.noreply.github.com>
2026-04-07 17:06:06 +0900

    옷 추천 함수 추가
    데이터 베이스에서 상의와 하의를 가져와 추천함.

--- recommend_clothes.py
+++ recommend_clothes.py
@@ -10,3 +10,20 @@ def get_target_level(temp):
     elif temp >= 0:  return 8
     elif temp >= -5: return 9
     else: return 10
+        
+#옷 추천 함수(current_temp 테스트 과정 중 입력으로 대체)
+def recommend_clothes(current_temp, clothes_db):
+    target_lv = get_target_level(current_temp)
+    
+    tops = [c for c in clothes_db if c['main_category'] == '상의']
+    bottoms = [c for c in clothes_db if c['main_category'] == '하의']
+    
+    print(f"목표 레벨: {target_lv}")
+    for top in tops:
+        for bottom in bottoms:
+            if top['temp_level'] == target_lv:
+                print(f"추천: {top['name']} + {bottom['name']}")
+
+#테스트 입력
+temp = int(input("현재 기온 입력(예: 14): "))
+recommend_clothes(temp, clothes_db)
