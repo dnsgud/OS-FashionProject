ea8ad5a63832d9e1c818e7d965b843ebb5566134
gichan8420 <gichan8420@gmail.com>
2026-04-08 21:19:12 +0900

    Supabase 접속을 위한 환경 변수 및 클라이언트 설정
    .env 파일을 읽어서 환경변수로 등록하고 이를 이용하여 이후 supabase 변수를 통해 서버와 통신한다.

--- imgproc.py
+++ imgproc.py
@@ -2,3 +2,8 @@ import os
 import traceback
 from dotenv import load_dotenv
 from supabase import create_client
+
+load_dotenv()
+url = os.getenv("SUPABASE_URL") # supabase 주소를 가져와 url에 저장
+key = os.getenv("SUPABASE_KEY") # api 키를 가져와 key 변수에 저장
+supabase = create_client(url, key) # 주소와 키 사용하여 실제 supabase 접속 객체 생성
