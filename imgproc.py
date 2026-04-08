9ee2d5c5644fbd5e733e5ff998de24a15aaa9739
gichan8420 <gichan8420@gmail.com>
2026-04-08 22:42:19 +0900

    DB 통합 함수 선언 및 파일 저장 경로 설정
    이미지 처리를 담당할 함수를 만들고 로컬에 있는 'test.jpg' 파일을 스토리지의 'test_upload.jpg'라는 이름으로 올리기로 파일명을 지정

--- imgproc.py
+++ imgproc.py
@@ -7,3 +7,7 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL") # supabase 주소를 가져와 url에 저장
 key = os.getenv("SUPABASE_KEY") # api 키를 가져와 key 변수에 저장
 supabase = create_client(url, key) # 주소와 키 사용하여 실제 supabase 접속 객체 생성
+
+def step2_db_integration():  # DB 연동 작업을 수행하는 함수를 정의
+    file_path = "test.jpg"  # 로컬에 저장되어 있는 이미지 파일의 경로를 지정
+    storage_path = "test_upload.jpg"  # Supabase 스토리지에 저장될 때 사용할 파일의 이름을 정함
