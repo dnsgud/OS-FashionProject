9de3bf561332c757f764b6405224b60fcfe7bf00
남현빈 <84217238+0301qls@users.noreply.github.com>
2026-04-10 16:13:38 +0900

    날씨 데이터 오류로 인한 수정

--- weather_service.py
+++ weather_service.py
@@ -6,12 +6,11 @@ load_dotenv()
 
 def fetch_weather():
     api_key = os.getenv("OPENWEATHER_API_KEY")
-    url = f"https://api.openweathermap.org/data/2.5/weather?q=Cheongju&appid={api_key}&units=metric"
+    # 청주(Cheongju) 데이터 요청
+    url = f"https://api.openweathermap.org/data/2.5/weather?q=Cheongju&appid={api_key}&units=metric&lang=kr" # lang=kr 추가 시 한국어 설명 가능
     
     response = requests.get(url)
-    data = response.json()
-    
-    return {
-        "temp": data['main']['temp'],
-        "status": data['weather'][0]['main']
-    }
+    if response.status_code == 200:
+        return response.json() 
+    else:
+        return None
